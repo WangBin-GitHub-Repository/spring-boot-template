01e401ccc98eab900dcc2b2fbfe9302b676f8d82
bin.wang <bin.wang@haitaichina.com>
2020-07-17 16:08:53 +0800

    Spring Initializr

---
+++ src/test/java/com/haitai/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitai.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
